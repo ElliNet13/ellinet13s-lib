df74c445c7b3a97ca3d2292fb3b624d95ace5a11
ElliNet13 <elliotmoose4@gmail.com>
2024-02-05 08:38:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author='ElliNet13',
     author_email='ellinet13@googlegroups.com',
     description='A library made by ElliNet13',
-    long_description='This library was made by ElliNet13 and will stay like that.',
+    long_description='This library was made by ElliNet13.',
     long_description_content_type='text/markdown',
     url='https://github.com/ElliNet13/ellinet13s-lib',
     packages=['ellinet13s-lib'],
